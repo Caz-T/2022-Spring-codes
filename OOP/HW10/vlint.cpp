5ed7babe719e45a4b4b1404982ad038cd18f9d51
Caz-T <89822413+Caz-T@users.noreply.github.com>
2022-05-12 18:54:23 +0800

    1st  commit

--- OOP/HW10/vlint.cpp
+++ OOP/HW10/vlint.cpp
@@ -2,5 +2,68 @@
 
 vlint::vlint()
 {
-
+    digit.emplace_back(0);
 }
+
+vlint::vlint(long long& another)
+{
+    if (another < 0) sign = false;
+    while (another)
+    {
+        digit.emplace_back(another % 10);
+        another = another / 10;
+    }
+}
+
+vlint::vlint(vlint &another) : sign(another.sign) {for (auto iter = another.digit.rend(); iter != another.digit.rbegin(); iter++) digit.emplace_back(*iter);}
+
+int vlint::operator[](int n) {return digit[n];}
+vlint vlint::operator+(vlint another)
+{
+    vlint ans(another);
+    if (!another.sign) for (auto& i : ans.digit) i = -i;
+    for (int i = ans.digit.size() + 1; i <= digit.size(); i++) ans.digit.emplace_back(0);
+    ans.digit.emplace_back(0);
+    for (int i = 0; i < ans.digit.size(); i++)
+    {
+        if (sign) ans.digit[i] += digit[i];
+        else ans.digit[i] -= digit[i];
+        if (ans.digit[i] > 10)
+        {
+            ans.digit[i] -= 10;
+            ans.digit[i + 1]++;
+        }
+        if (ans.digit[i] < 0)
+        {
+            ans.digit[i] += 10;
+            ans.digit[i + 1]--;
+        }
+    }
+    if (*ans.digit.end() < 0)
+    {
+        ans.sign = false;
+        for (auto& i : ans.digit) i = -i;
+        for (int i = 0; i < ans.digit.size() - 1; i++)
+        {
+            if (ans.digit[i] > 10)
+            {
+                ans.digit[i] -= 10;
+                ans.digit[i + 1]++;
+            }
+            if (ans.digit[i] < 0)
+            {
+                ans.digit[i] += 10;
+                ans.digit[i + 1]--;
+            }
+        }
+    }
+    if (*ans.digit.end() == 0) ans.digit.pop_back();
+    return ans;
+}
+vlint vlint::operator-(vlint another)
+{
+    vlint temp(another);
+    temp.sign = not temp.sign;
+    return *this + temp;
+}
+
--- OOP/HW10/vlint.h
+++ OOP/HW10/vlint.h
@@ -8,18 +8,20 @@ class vlint // vlint stands for very long int
 {
 public:
     vlint();
-
+    vlint(long long& another);
+    vlint(vlint& another);
     ~vlint() = default;
 
     int operator[](int n);
-    vlint operator+ (vlint& another);
-    vlint operator+ (long long& another);
-    vlint operator- (vlint& another);
-    vlint operator- (long long& another);
+    vlint operator+ (vlint another);
+    vlint operator- (vlint another);
 
     vector<int> digit; //digit[0] is the right-most digit
+    bool sign = true; // true for positive
 };
 
+/*
 vlint operator+ (long long& a, vlint& b);
 vlint operator- (long long& a, vlint& b);
+ */
 #endif //HW10_VLINT_H
